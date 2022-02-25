596c62b21a3b9a86632d825b13c60cbb66925944
teekaygithub <tomoyuki.kato@yahoo.com>
2022-02-25 00:10:04 -0800

    Individual cart items stored in localStorage

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // Initialize the cart count icon with current count, if any
     var cart = localStorage.getItem('cart');
     if (cart) {
         var temp = JSON.parse(cart);
@@ -6,14 +7,15 @@ $(document).ready(function() {
     }
 });
 
-$(".cart-button").on("click", function() {
-    console.log('Add to cart button pressed');
+$(".cart-button").on("click", function () {
+
+    // Cart details
     var cart = localStorage.getItem('cart');
 
     if (cart) {
         var temp = JSON.parse(cart);
         console.log('Existing cart, quantity: ' + temp.totalQuantity);
-        temp.totalQuantity+=1;
+        temp.totalQuantity += 1;
         localStorage.setItem('cart', JSON.stringify(temp));
         $(".cart-count").text(temp.totalQuantity);
     } else {
@@ -23,4 +25,38 @@ $(".cart-button").on("click", function() {
         localStorage.setItem('cart', JSON.stringify(cart));
         $(".cart-count").text("1");
     }
+
+    // Cart items
+
+    // First get the details of the product being added
+    var temp = $(this).attr("id").split('-');
+    var id = temp[temp.length - 1];
+    // console.log(id);
+
+    var product = {
+        name: $("#item-name-" + id).text(),
+        price: $("#item-price-" + id).text(),
+        imageUrl: $("#item-image-" + id).attr("src"),
+        quantity: "1"
+    };
+    // console.log(product);
+
+    // Second add the item to the array of existing cart items
+    var cartItems = JSON.parse(localStorage.getItem('cart-items') || "[]");
+    var existing = cartItems.find(function(item) {
+        return item.name === product.name;
+    });
+
+    if (existing) {
+        cartItems.forEach(function(item) {
+            if (item.name === product.name) {
+                let qty = parseInt(item.quantity) + 1;
+                item.quantity = qty.toString();
+            }
+        })
+    } else {
+        cartItems.push(product);
+    }
+    
+    localStorage.setItem('cart-items', JSON.stringify(cartItems));
 });
