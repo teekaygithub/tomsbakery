8b87738d65d5dc2e3305ed3a829e73bd110154d4
teekaygithub <tomoyuki.kato@yahoo.com>
2022-02-26 17:47:22 -0800

    Cart details page: checkout button and display cart subtotals. Default styling to all buttons. Removing cart item will recompute the subtotal.

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -2,20 +2,24 @@ $(document).ready(function () {
     // Initialize the cart count icon with current count, if any
     var cart = JSON.parse(localStorage.getItem('cart'));
     if (cart && cart.totalQuantity > 0) {
-        // var temp = JSON.parse(cart);
         $(".cart-count").text(cart.totalQuantity);
         $("#cart-empty").hide();
 
-        // Populate the shopping cart page (/cart)
+        // Populate the shopping cart page (/cart) and compute the subtotal
         let cartItemsList = JSON.parse(localStorage.getItem('cart-items'));
+        let subtotal = 0;
         cartItemsList.forEach((item) => {
             let image = "<div class=\"cart-item-image\"><img src=\"" + item.imageUrl + "\" ></div>";
             let name = "<div class=\"cart-item-name\"><p>" + item.name + "</p></div>";
-            let buttons = "<div class=\"cart-item-buttons\"><input type=\"number\" min=\"1\" max=\"6\"><button>Update</button><button class=\"cart-item-remove\">Remove</button></div>"
+            let buttons = "<div class=\"cart-item-buttons\"><input type=\"number\" min=\"1\" max=\"6\" placeholder=\"1\"><button>Update</button><button class=\"cart-item-remove\">Remove</button></div>"
             let price = "<div class=\"cart-item-price-qty\"><p>" + item.price + "</p><p>" + item.quantity + " x " + item.price + "</p></div>";
             let prod = "<div class=\"cart-item\" id=\"cart-item-" + item.ID + "\">" + image + name + buttons + price + "</div>";
             $("#cart-hasitem").append(prod);
+            subtotal += parseInt(item.quantity) * parseFloat(item.price.substring(1));
         });
+
+        console.log("Subtotal:" + subtotal);
+        totalsAndCheckoutButton(subtotal.toFixed(2));
     } else {
         console.log("Cart is empty");
         $("#cart-hasitem").hide();
@@ -29,6 +33,7 @@ $(document).ready(function () {
     
 }); // end document ready
 
+// Expected to be called from the menu page and the item-detail page
 function addToCart() {
     $(".cart-button").on("click", function () {
 
@@ -84,6 +89,7 @@ function addToCart() {
     });
 }
 
+// Expected to be called from the shopping-cart details page
 function removeFromCart() {
     $(".cart-item-remove").on("click", function () {
         console.log('Remove button clicked');
@@ -122,6 +128,25 @@ function removeFromCart() {
         } else {
             localStorage.setItem('cart', JSON.stringify(modifiedCart));
             $(".cart-count").text(modifiedCart.totalQuantity);
+            let newSubtotal = computeSubtotal(modifiedList);
+            $("#subtotal").text("Your total: $" + newSubtotal.toString());
         }
     });
+}
+
+// Adds the subtotal and checkout button for the /cart page
+function totalsAndCheckoutButton(subtotal) {
+    let totals = "<div id=\"subtotal\">Your total: $" + subtotal.toString() + "</div>"
+    let checkoutBtn = "<div><a href=\"#\"><button>Checkout</button></a></div>"
+    let component = "<div class=\"cart-checkout-container\">" + totals + checkoutBtn + "</div>";
+    $("#cart-hasitem").append(component);
+}
+
+function computeSubtotal(itemList) {
+    let subtotal = 0;
+    itemList.forEach((item) => {
+        subtotal += parseInt(item.quantity) * parseFloat(item.price.substring(1));
+    });
+
+    return subtotal.toFixed(2);
 }
