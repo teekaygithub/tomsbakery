ca07dbaab50013804da9e1ff58ec16a565bef2e5
teekaygithub <tomoyuki.kato@yahoo.com>
2022-03-06 00:07:50 -0800

    Image carousel responsiveness for small screens

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,16 +1,18 @@
 $(document).ready(() => {
     let imgCount = $(".carousel-slide-img").length;
     console.log("carousel img count: " + imgCount.toString());
+    console.log("Screen width: " + window.outerWidth);
     showSlides(imgCount);
 });
 
 function showSlides(imgCount) {
 
-    const imgWidth = 700;
+    imgWidth = parseInt($(".carousel-slide-img").width());
+    console.log("img width: " + imgWidth);
 
     $(".next").on("click", () => {
         let offset = parseInt($("#carousel-slides").css("transform").split(',')[4]);
-        let newOffset = (offset - 700) <= -1 * imgWidth * imgCount ? offset : offset - 700;
+        let newOffset = (offset - imgWidth) <= -1 * imgWidth * imgCount ? offset : offset - imgWidth;
         let translate = "translateX(" + newOffset.toString() + "px)";
         
         $("#carousel-slides").css("transform", translate);
@@ -18,7 +20,7 @@ function showSlides(imgCount) {
 
     $(".prev").on("click", () => {
         let offset = parseInt($("#carousel-slides").css("transform").split(',')[4]);
-        let newOffset = (offset + 700) > 0 ? offset : offset + 700;
+        let newOffset = (offset + imgWidth) > 0 ? offset : offset + imgWidth;
         let translate = "translateX(" + newOffset.toString() + "px)";
 
         $("#carousel-slides").css("transform", translate);
