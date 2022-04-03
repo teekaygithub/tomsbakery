ca1d50300de8522c78a63b8e90c4faa02348bf38
teekaygithub <tomoyuki.kato@yahoo.com>
2022-04-03 16:28:54 -0700

    Initial integration of Okta

--- src/main/java/com/tkato/tkbakery/controllers/HomeController.java
+++ src/main/java/com/tkato/tkbakery/controllers/HomeController.java
@@ -4,6 +4,8 @@ import com.tkato.tkbakery.models.CustomProduct;
 import com.tkato.tkbakery.services.ProductService;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,12 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, @AuthenticationPrincipal OidcUser user) {
         Page<CustomProduct> featured = service.findTop3();
         model.addAttribute("featured", featured);
+        if (user != null) {
+            model.addAttribute("userName", user.getFullName());
+        }
         return "index";
     }
 }
--- src/main/java/com/tkato/tkbakery/security/SecurityConfig.java
+++ src/main/java/com/tkato/tkbakery/security/SecurityConfig.java
@@ -1,29 +1,13 @@
 package com.tkato.tkbakery.security;
 
-import com.tkato.tkbakery.services.UserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(encoder);
-    }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,5 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             // .antMatchers("/users/**").permitAll() // temporary, for development purposes
             // .anyRequest().authenticated();
             .anyRequest().permitAll();
+        // http.oauth2Login();
+        // http.oauth2Client();
     }
 }
